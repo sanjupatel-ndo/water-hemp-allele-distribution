3c004383a6abb73cde9aed8dccf95913150641ba
Sanju Patel <sanjupatel@uchicago.edu>
2026-09-16 15:51:03 -0500

    renamed

---
+++ location-resampling/meansd-pipeline.py
@@ -0,0 +1,13 @@
+"""
+Kreiner Lab - September 14th 2026
+Resampling pipeline
+Primary contributor(s): Sanju Patel
+
+Objective: Pull environmental data for samples with varying degrees of location 
+uncertainty
+
+Methodology: For each 
+
+Data requirements:
+    Inputs: Latitude, Longitude
+"""
