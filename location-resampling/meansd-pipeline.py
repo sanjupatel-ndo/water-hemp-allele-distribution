bedd9239b853ff1ddde6cfaf2b73ad48b5f87ef5
Sanju Patel <sanjupatel@uchicago.edu>
2026-09-16 16:04:56 -0500

    added initial data set

--- location-resampling/meansd-pipeline.py
+++ location-resampling/meansd-pipeline.py
@@ -6,7 +6,8 @@ Primary contributor(s): Sanju Patel
 Objective: Pull environmental data for samples with varying degrees of location 
 uncertainty
 
-Methodology: For each 
+Methodology: For each sample, draw a circle where the sample may have been
+collected. For each environmental variable, randomly sample some number of 
 
 Data requirements:
     Inputs: Latitude, Longitude
