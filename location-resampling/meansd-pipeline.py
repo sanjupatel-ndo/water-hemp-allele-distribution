4fffad7ed62a7a06d6f05f76621de001e6f679ac
Sanju Patel <sanjupatel@uchicago.edu>
2026-09-16 22:07:28 -0500

    updating methodology

--- location-resampling/meansd-pipeline.py
+++ location-resampling/meansd-pipeline.py
@@ -8,7 +8,11 @@ uncertainty
 
 Methodology: For each sample, draw a circle where the sample may have been
 collected. For each environmental variable, randomly sample some number of 
+observations within this radius. For this first pass, report the mean and 
+standard deviation of the sampling distribution for each environmental variable, 
+for each sample.
 
 Data requirements:
-    Inputs: Latitude, Longitude
+    Inputs: Waterhemp sample with a latitude and longtitude
 """
+
