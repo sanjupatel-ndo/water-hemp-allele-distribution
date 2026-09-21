7878f560eccb96164d0c8d92ff8f0c3cc28f01bd
Sanju Patel <sanjupatel@uchicago.edu>
2026-09-21 15:26:56 -0500

    built most of buffer pipeline

--- location-resampling/meansd-pipeline.py
+++ location-resampling/meansd-pipeline.py
@@ -1,7 +1,7 @@
 """
 Kreiner Lab - September 14th 2026
 Resampling pipeline
-Primary contributor(s): Sanju Patel
+Primary contributor(s): Sanju Patel, Julian Huang
 
 Objective: Pull environmental data for samples with varying degrees of location 
 uncertainty
@@ -15,4 +15,36 @@ for each sample.
 Data requirements:
     Inputs: Waterhemp sample with a latitude and longtitude
 """
+#Step 1: For each sample, create an area that represents where the sample was
+#collected, taking into account differing levels of location uncertainty.
 
+import pandas as pd
+import geopandas as gpd
+from shapely.geometry import Point, Polygon
+
+def create_buffers(obs_df: pd.DataFrame, radius: float) -> gpd.GeoDataFrame:
+    """
+    Creates 1 km vector buffers around observation points. Assumes observations 
+    from GBIF and has 'decimalLongitude' and 'decimalLatitude' keys
+
+    Args:
+        obs_df (pd.DataFrame): DataFrame containing GBIF observation coordinates 
+                               with 'decimalLongitude' and 'decimalLatitude' 
+                               columns.
+
+    Returns:
+        gpd.GeoDataFrame: A GeoDataFrame with original points and their 
+        corresponding 1 km buffers.
+    """
+    geometry = [Point(lon, lat) for lon, lat in zip(obs_df['decimalLongitude'], obs_df['decimalLatitude'])]
+    gdf = gpd.GeoDataFrame(obs_df, geometry=geometry, crs="EPSG:4326")
+
+    gdf_utm = gdf.to_crs("EPSG:32616")
+    gdf_utm["uncertainty_buffer"] = gdf_utm.geometry.buffer(gdf_utm["coordinateUncertaintyInMeters"])
+    return gdf_utm
+
+gbif_observations_file_path = '..\\1000m_amaranthus_with_proportions.csv'
+df = pd.read_csv(gbif_observations_file_path, sep='\t')
+
+gdf_utm = create_buffers(df, radius=1000)
+gdf_utm.head()
